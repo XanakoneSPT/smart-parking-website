3bc06b74db281d3238b88dc8d96e838e6c2ddef7
XanakoneSPT <xs.xanakone@gmail.com>
2025-06-13 08:37:51 +0700

    modified:   src/services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Make sure this URL matches your backend server
-export const API_URL = 'http://raspberrypi.local:8000/';
+export const API_URL = 'http://localhost:8000/';
 
 // Add authorization header to requests
 const getHeaders = () => {
